e5d36677322ea5724767917c8c5107c1f7096265
Marcus D. R. Klarqvist <mk819@cam.ac.uk>
2019-03-20 19:38:56 +0000

    pbwt abstraction symbols

--- main.cpp
+++ main.cpp
@@ -22,46 +22,54 @@
 const uint8_t BCF_UNPACK_TOMAHAWK[3] = {2, 0, 1};
 #define BCF_UNPACK_GENOTYPE(A) BCF_UNPACK_TOMAHAWK[(A >> 1)]
 
+template <int n_symbols = 2>
 class PBWT {
 public:
     //PBWT() : n_samples(0), prev(nullptr), ppa(nullptr), n_q1(0), n_q2(0), q1(nullptr), q2(nullptr){}
     PBWT() :
         n_samples(0), n_steps(0),
-        prev(nullptr), ppa(nullptr),
-        n_q1(0), n_q2(0),
-        q1(nullptr), q2(nullptr)
+        prev(nullptr), ppa(nullptr)
     {
-
+        for(int i = 0; i < n_symbols; ++i) {
+            queue[i] = nullptr;
+        }
+        memset(n_queue, 0, sizeof(uint32_t)*n_symbols);
     }
 
     PBWT(int64_t n_samples) :
         n_samples(n_samples),
         n_steps(0),
         prev(new uint8_t[n_samples]),
-        ppa(new uint32_t[n_samples]),
-        n_q1(0), n_q2(0),
-        q1(new uint32_t[n_samples]), q2(new uint32_t[n_samples])
+        ppa(new uint32_t[n_samples])
     {
+
+        for(int i = 0; i < n_symbols; ++i) {
+            queue[i] = new uint32_t[n_samples];
+        }
+        memset(n_queue, 0, sizeof(uint32_t)*n_symbols);
         reset();
     }
 
-    virtual ~PBWT() {
+    ~PBWT() {
         delete[] prev;
         delete[] ppa;
-        delete[] q1;
-        delete[] q2;
+        for(int i = 0; i < n_symbols; ++i) {
+            delete[] queue[i];
+        }
     }
 
     void Initiate(int64_t n_samples) {
         delete[] prev;
         delete[] ppa;
-        delete[] q1;
-        delete[] q2;
+        for(int i = 0; i < n_symbols; ++i) {
+            delete[] queue[i];
+        }
         this->n_samples = n_samples;
         prev = new uint8_t[n_samples];
         ppa  = new uint32_t[n_samples];
-        q1   = new uint32_t[n_samples];
-        q2   = new uint32_t[n_samples];
+        for(int i = 0; i < n_symbols; ++i) {
+            queue[i] = new uint32_t[n_samples];
+        }
         reset();
     }
 
@@ -74,18 +82,19 @@ public:
         if(prev != nullptr && n_samples) {
             memset(prev, 0, sizeof(uint8_t)*n_samples);
         }
-        n_q1 = n_q2 = 0;
+        //n_q1 = n_q2 = 0;
+        memset(n_queue, 0, sizeof(uint32_t)*n_symbols);
     }
 
-    virtual int Update(const int* arr) {
-        n_q1 = n_q2 = 0;
+    int Update(const int* arr) {
+        //n_q1 = n_q2 = 0;
+        memset(n_queue, 0, sizeof(uint32_t)*n_symbols);
 
         for(int i = 0; i < n_samples; ++i) {
-            uint8_t gt = BCF_UNPACK_GENOTYPE(arr[ppa[i]]);
-            switch(gt) {
-            case(0): q1[n_q1++] = ppa[i]; break;
-            case(1): q2[n_q2++] = ppa[i]; break;
-            default: std::cerr << "error!!!" << std::endl; exit(1);
+            const uint8_t& gt = BCF_UNPACK_GENOTYPE(arr[ppa[i]]);
+            for(int j = 0; j < n_symbols; ++j) {
+                if(gt == j)
+                    queue[j][n_queue[j]++] = ppa[i];
             }
             prev[i] = gt;
             //std::cerr << " " << (int)gt;
@@ -93,8 +102,46 @@ public:
         //std::cerr << std::endl;
 
         uint32_t of = 0;
-        for(int i = 0; i < n_q1; ++i, ++of) ppa[of] = q1[i];
-        for(int i = 0; i < n_q2; ++i, ++of) ppa[of] = q2[i];
+        for(int j = 0; j < n_symbols; ++j) {
+            for(int i = 0; i < n_queue[j]; ++i, ++of) ppa[of] = queue[j][i];
+        }
+        //for(int i = 0; i < n_q2; ++i, ++of) ppa[of] = q2[i];
+        assert(of == n_samples);
+        // debug should be sorted here
+        //for(int i = 0; i < n_samples; ++i) {
+         //   std::cerr << " " << (int)BCF_UNPACK_GENOTYPE(arr[ppa[i]]);
+        //}
+        //std::cerr << std::endl;
+        ++n_steps;
+
+        return(1);
+    }
+
+    template <class T>
+    int Update(const T* arr, uint32_t stride = 1) {
+        //n_q1 = n_q2 = 0;
+        memset(n_queue, 0, sizeof(uint32_t)*n_symbols);
+
+        for(int i = 0; i < n_samples; ++i) {
+            const uint8_t& gt = BCF_UNPACK_GENOTYPE(arr[ppa[i] * stride]);
+            assert(gt < n_symbols);
+            for(int j = 0; j < n_symbols; ++j) {
+                if(gt == j)
+                    queue[j][n_queue[j]++] = ppa[i];
+            }
+            prev[i] = gt;
+            //std::cerr << " " << (int)gt;
+        }
+        //assert(p == n_samples);
+        //std::cerr << std::endl;
+
+        uint32_t of = 0;
+        for(int j = 0; j < n_symbols; ++j) {
+            for(int i = 0; i < n_queue[j]; ++i, ++of)
+                ppa[of] = queue[j][i];
+        }
+        //for(int i = 0; i < n_q2; ++i, ++of) ppa[of] = q2[i];
+        //std::cerr << "of=" << of << "/" << n_samples << std::endl;
         assert(of == n_samples);
         // debug should be sorted here
         //for(int i = 0; i < n_samples; ++i) {
@@ -121,52 +168,8 @@ public:
     uint64_t n_steps;
     uint8_t* prev;
     uint32_t* ppa;
-    uint32_t n_q1, n_q2;
-    uint32_t *q1, *q2;
-};
-
-class BitmapPBWT : public PBWT {
-public:
-    BitmapPBWT(int64_t n_samples) : PBWT(n_samples), n_bitmaps(0), bitmaps(nullptr) {
-        n_bitmaps = std::ceil(n_samples / 64.0);
-        bitmaps = new uint64_t[n_bitmaps];
-        memset(bitmaps,0,sizeof(uint64_t)*n_bitmaps);
-    }
-
-    ~BitmapPBWT() {
-        delete[] bitmaps;
-    }
-
-    void resetBitmaps() {
-        memset(bitmaps,0,sizeof(uint64_t)*n_bitmaps);
-    }
-
-    int Update(const int* arr) override {
-        resetBitmaps();
-
-        for(int i = 0; i < n_samples; ++i) {
-            uint64_t gt = BCF_UNPACK_GENOTYPE(arr[ppa[i]]);
-            switch(gt) {
-            case(0): q1[n_q1++] = ppa[i]; break;
-            case(1): q2[n_q2++] = ppa[i]; break;
-            default: std::cerr << "error!!!" << std::endl; exit(1);
-            }
-            bitmaps[ppa[i] / 64] |= ((gt == 1) << (ppa[i] / 64 & 4));
-        }
-
-        uint32_t of = 0;
-        for(int i = 0; i < n_q1; ++i, ++of) ppa[of] = q1[i];
-        for(int i = 0; i < n_q2; ++i, ++of) ppa[of] = q2[i];
-        assert(of == n_samples);
-        n_q1 = n_q2 = 0;
-        ++n_steps;
-
-        return(1);
-    }
-
-public:
-    int32_t n_bitmaps;
-    uint64_t* bitmaps;
+    uint32_t n_queue[n_symbols];
+    uint32_t* queue[n_symbols];
 };
 
 int ZstdCompress(const uint8_t* in, uint32_t n_in, uint8_t* out, uint32_t out_capacity, const int32_t c_level = 1) {
@@ -192,10 +195,10 @@ void ReadVcfGT (const std::string& filename) {
     //bcf1_t *rec = bcf_init1();
 
     //const uint32_t n_samples = hr->n[2];
-    int mgt_arr = 0;
-    int* gt_arr = nullptr;
-    int* gt1_a = new int[uint32_t(2.5*reader->n_samples_)];
-    int* gt2_a = new int[uint32_t(2.5*reader->n_samples_)];
+    //int mgt_arr = 0;
+    //int* gt_arr = nullptr;
+   // int* gt1_a = new int[uint32_t(2.5*reader->n_samples_)];
+    //int* gt2_a = new int[uint32_t(2.5*reader->n_samples_)];
 
     uint64_t n_out = 0, n_in = 0, n_data = 0, n_out_zstd = 0;
     uint8_t* data_buffer = new uint8_t[10000000];
@@ -205,19 +208,21 @@ void ReadVcfGT (const std::string& filename) {
     pil::RangeCoder rc1, rc2;
     pil::FrequencyModel<2>* fmodel1 = new pil::FrequencyModel<2>[2048];
     pil::FrequencyModel<2>* fmodel2 = new pil::FrequencyModel<2>[2048];
+    pil::FrequencyModel<2>* fmodel1_hard = new pil::FrequencyModel<2>[2048];
+    pil::FrequencyModel<2>* fmodel2_hard = new pil::FrequencyModel<2>[2048];
     rc1.SetOutput(out1_buffer);
     rc1.StartEncode();
     rc2.SetOutput(out2_buffer);
     rc2.StartEncode();
 
-    PBWT* pbwt = new PBWT[2];
+    PBWT<2>* pbwt = new PBWT<2>[2];
     pbwt[0].Initiate(reader->n_samples_);
     pbwt[1].Initiate(reader->n_samples_);
 
     uint32_t n_processed = 0;
     uint32_t n_lines = 0;
 
-    while (reader->Next(0)) {
+    while (reader->Next()) {
         //const char* chrom = bcf_seqname(hr,line) ;
         //if (!p->chrom) p->chrom = strdup (chrom) ;
         //else if (strcmp (chrom, p->chrom)) break ;
@@ -232,23 +237,25 @@ void ReadVcfGT (const std::string& filename) {
 
         // get a copy of GTs
         //mgt_arr = 10*reader->n_samples_;
-        int ngt = bcf_get_genotypes(reader->header_, reader->bcf1_, &gt_arr, &mgt_arr);
+        //int ngt = bcf_get_genotypes(reader->header_, reader->bcf1_, &gt_arr, &mgt_arr);
 
 
-        if (ngt <= 0) continue;  // it seems that -1 is used if GT is not in the FORMAT
-        if (ngt != 2*reader->n_samples_) {
-            std::cerr << "not multiple of 2*n_s" << std::endl;
-            continue;
-        }
+        //if (ngt <= 0) continue;  // it seems that -1 is used if GT is not in the FORMAT
+        //if (ngt != 2*reader->n_samples_) {
+        //    std::cerr << "not multiple of 2*n_s" << std::endl;
+        //    continue;
+        //}
+
+
         //memcpy(gt_a, gt_arr, ngt*sizeof(int));
-        int j = 0;
-        for(int i = 0; i < 2*reader->n_samples_; i += 2, ++j) {
-            gt1_a[j] = gt_arr[i+0];
-            gt2_a[j] = gt_arr[i+1];
-        }
+        //int j = 0;
+        //for(int i = 0; i < 2*reader->n_samples_; i += 2, ++j) {
+        //    gt1_a[j] = gt_arr[i+0];
+         //   gt2_a[j] = gt_arr[i+1];
+        //}
 
-        pbwt[0].Update(gt1_a);
-        pbwt[1].Update(gt2_a);
+        pbwt[0].Update(reader->bcf1_->d.fmt[0].p+0, 2);
+        pbwt[1].Update(reader->bcf1_->d.fmt[0].p+1, 2);
         ++n_processed;
         ++n_lines;
         // encode
@@ -261,38 +268,47 @@ void ReadVcfGT (const std::string& filename) {
         }
         */
 
-/*
-        if(pbwt[0].n_q2 >= 100) {
-            for(int i = 0; i < reader->n_samples_; ++i) {
-                data_buffer[n_data++] = pbwt[0].prev[i];
-            }
+
+        if(pbwt[0].n_queue[1] >= 100) {
+           uint16_t s = 0;
+           for(int i = 0; i < reader->n_samples_; ++i) {
+               fmodel1_hard[s].EncodeSymbol(&rc1, pbwt[0].prev[i]);
+               s <<= 1;
+               s |= (pbwt[0].prev[i] == 1);
+               s &= 2047;
+               _mm_prefetch((const char *)&fmodel1_hard[s], _MM_HINT_T0);
+               //data_buffer[n_data++] = pbwt[0].prev[i];
+           }
         }
         else
-*/
+
         {
             uint16_t s = 0;
             for(int i = 0; i < reader->n_samples_; ++i) {
                 fmodel1[s].EncodeSymbol(&rc1, pbwt[0].prev[i]);
                 s <<= 1;
                 s |= (pbwt[0].prev[i] == 1);
-                s &= 1023;
-                s <<= 1;
-                s |= (i > 0 ? 0 : (pbwt[0].prev[i] == pbwt[0].prev[i-1]));
-                //s &= 127;
-                //s <<= 3;
-                //s |= i / (313);
+                s &= 2047;
                 _mm_prefetch((const char *)&fmodel1[s], _MM_HINT_T0);
                 //data_buffer[n_data++] = pbwt[0].prev[i];
             }
         }
 
-/*
-        if(pbwt[1].n_q2 >= 100) {
-            for(int i = 0; i < reader->n_samples_; ++i) {
-                data_buffer[n_data++] = pbwt[1].prev[i];
-            }
-        } else
-*/
+
+        if(pbwt[1].n_queue[1] >= 100) {
+           uint16_t s = 0;
+           for(int j = 0; j < reader->n_samples_; ++j) {
+               //std::cerr << (int)pbwt[1].prev[j] << " ";
+               fmodel2_hard[s].EncodeSymbol(&rc2, pbwt[1].prev[j]);
+               s <<= 1;
+               s |= (pbwt[1].prev[j] == 1);
+               s &= 2047;
+               _mm_prefetch((const char *)&fmodel2_hard[s], _MM_HINT_T0);
+               //data_buffer[n_data++] = pbwt[1].prev[j];
+           }
+        }
+        else
+
         {
             uint16_t s = 0;
             for(int j = 0; j < reader->n_samples_; ++j) {
@@ -300,12 +316,7 @@ void ReadVcfGT (const std::string& filename) {
                 fmodel2[s].EncodeSymbol(&rc2, pbwt[1].prev[j]);
                 s <<= 1;
                 s |= (pbwt[1].prev[j] == 1);
-                s &= 1023;
-                s <<= 1;
-                s |= (j > 0 ? 0 : (pbwt[1].prev[j] == pbwt[1].prev[j-1]));
-                //s &= 127;
-                //s <<= 3;
-                //s |= j / (313);
+                s &= 2047;
                 _mm_prefetch((const char *)&fmodel2[s], _MM_HINT_T0);
                 //data_buffer[n_data++] = pbwt[1].prev[j];
             }
@@ -336,8 +347,9 @@ void ReadVcfGT (const std::string& filename) {
             rc1.SetOutput(out1_buffer);
             rc1.StartEncode();
             //std::cerr << "RC1 " << n_lines << " Compressed=" << n_in << "->" << n_out << "(" << (float)n_in/n_out << "-fold)" << std::endl;
-            delete[] fmodel1;
+            delete[] fmodel1; delete[] fmodel1_hard;
             fmodel1 = new pil::FrequencyModel<2>[2048];
+            fmodel1_hard = new pil::FrequencyModel<2>[2048];
             pbwt[0].reset();
 
             n_out += rc2.OutSize();
@@ -345,8 +357,9 @@ void ReadVcfGT (const std::string& filename) {
             rc2.SetOutput(out2_buffer);
             rc2.StartEncode();
             std::cerr << "RC " << n_lines << " Compressed=" << n_in << "->" << n_out << "(" << (float)n_in/n_out << "-fold)" << std::endl;
-            delete[] fmodel2;
+            delete[] fmodel2; delete[] fmodel2_hard;
             fmodel2 = new pil::FrequencyModel<2>[2048];
+            fmodel2_hard = new pil::FrequencyModel<2>[2048];
             pbwt[1].reset();
             n_lines = 0;
         }
@@ -376,11 +389,12 @@ void ReadVcfGT (const std::string& filename) {
     }
 
     delete[] fmodel1; delete[] fmodel2;
-    delete[] gt_arr;
+    delete[] fmodel1_hard; delete[] fmodel2_hard;
+    //delete[] gt_arr;
     delete[] data_buffer;
     delete[] out1_buffer; delete[] out2_buffer;
     delete[] zstd_buffer;
-    delete[] gt1_a; delete[] gt2_a;
+    //delete[] gt1_a; delete[] gt2_a;
     delete[] pbwt;
 }
 
